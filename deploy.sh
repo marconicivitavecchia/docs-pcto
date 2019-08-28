6f34e54fc0b9a49a0f2e7f85e9e44c4e49201a64
wbigger <wbigger@gmail.com>
2019-08-28 16:32:48 +0200

    change default commit message

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ rm -rf /tmp/book/* # this won't delete the .git directory
 cp -rp book/* /tmp/book/
 cd /tmp/book
 git add -A
-git commit -m'new book message'
+git commit -m'update gh pages'
 git push origin gh-pages
 cd -
